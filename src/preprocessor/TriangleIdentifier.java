4d712b73f7dfa3538866f78979516f2104c448da
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-11-25 23:31:42 -0500

    Maybe Added Test

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -38,9 +38,54 @@ public class TriangleIdentifier
 	private void computeTriangles()
 	{
 		// TODO
+
+		//begin with a segment from given segments
+		for (int i = 0; i < _segments.size()-1; i++ ) {
+
+
+			//for each segment AFTER that segment, compare
+			for (int j = i+1; j < _segments.size(); j++) {
+
+				//for the segment after the previous one
+				for(int k = j+1; k < _segments.size(); k++) {
+
+					//get the three segments in the map
+					Segment seg1 = _segments.get(i);
+					Segment seg2 = _segments.get(j);
+					Segment seg3 = _segments.get(k);
+
+
+					//if any of the endpoints of the segments lie on other segments
+					//any of them intersect with any of the others
+					if(!seg1.pointLiesBetweenEndpoints(seg2.getPoint1()) || !seg1.pointLiesBetweenEndpoints(seg2.getPoint2()) 
+							|| !seg1.pointLiesBetweenEndpoints(seg3.getPoint1()) || !seg1.pointLiesBetweenEndpoints(seg3.getPoint2())
+							|| !seg2.pointLiesBetweenEndpoints(seg3.getPoint1()) || !seg2.pointLiesBetweenEndpoints(seg3.getPoint2())) {
+						
+						
+						//check that the segments are not on the same slope
+						if(!seg1.coincideWithoutOverlap(seg2) || !seg1.coincideWithoutOverlap(seg3) || !seg2.coincideWithoutOverlap(seg3)) {
+							
+						}
+
+					}
+
+
+
+
+
+
+				}
+			}
+
+		}
+
+
 		//get three segments
 		//if two segments share a vertex and have an angle of greater than 0
 		//and that works for all three segment verticies
 		//create a new list of segments and create a new triangle and add to database
+
+		//if the angle with any of the segments is 180 degrees then it is not a triangle
+
 	}
 }
---
+++ test/preprocessor/AngleIdentifierTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AngleIdentifierTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
